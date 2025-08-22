23d2cb35a1dc45286745987b6db79a3626547d81
Abdul Rahman <abdulrahman161004@gmail.com>
2025-08-22 23:14:04 +0530

    feat: setup routing with react-router-dom
    - Added Navbar component to App layout
    - Configured Routes and Route for Home, Product, About, and Contact pages
    - Applied basic styling for centered content

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
+import Navbar from './components/Navbar'
+import Home from './pages/Home'
+import Contact from './pages/Contact'
+import About from './pages/About'
+import Product from './pages/Product'
+import { Routes, Route } from 'react-router-dom'
+
 const App = () =>
 {
   return (
     <div>
+      <Navbar />
+      <div className="flex items-center justify-center min-h-screen text-7xl">
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/product' element={<Product />} />
+          <Route path='/about' element={<About />} />
+          <Route path='/contact' element={<Contact />} />
+        </Routes>
+      </div>
     </div>
   )
 }
