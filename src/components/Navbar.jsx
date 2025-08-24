899902d8866ca64b4432a936b49ecd938dcac550
Abdul Rahman <abdulrahman161004@gmail.com>
2025-08-24 16:48:18 +0530

    feat: updated Navbar with logo and navigation links
    - Imported new logo (logo.jpg) from assets and displayed as a rounded image
    - Added navigation links (Home, Products, About, Contact) using <Link> from react-router-dom
    - Styled navbar with flexbox, spacing, and shadow for modern UI
    - Added 'Get Started' button with hover transition for better interactivity

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
-
-const Navbar = () => {
+import logo from '../assets/logo.jpg'
+import { Link } from 'react-router-dom'
+const Navbar = () =>
+{
   return (
-    <div>
-
+    <div className='w-full flex justify-between items-center px-6 py-3'>
+      <img src={logo} alt="logo_img" className='w-18 rounded-full ' />
+      <ul className='flex gap-12 rounded-4xl shadow-2xl px-10 py-4 '>
+        <Link to="/"><li>Home</li></Link>
+        <Link to="/Products"><li>Products</li></Link>
+        <Link to="/About"><li>About</li></Link>
+        <Link to="/Contact"><li>Contact</li></Link>
+      </ul>
+      <button className='bg-black text-white rounded-2xl px-4 py-2 hover:bg-gray-800 transition'>Get Started</button>
     </div>
   )
 }
