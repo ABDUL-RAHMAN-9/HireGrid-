4fae5a3c1b94987c3eb248a798d99e11371ea058
Abdul Rahman <abdulrahman161004@gmail.com>
2025-08-27 23:28:57 +0530

    Added Button text size with text-xl

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -3,12 +3,12 @@ import React from 'react'
 const Button = ({ text, onClick }) =>
 {
     return (
-        <div>
-            <button className="bg-black text-white rounded-2xl px-4 py-2 hover:bg-gray-800 transition"
-                onClick={onClick}>
-                {text}
-            </button>
-        </div>
+        <button
+            className="bg-black text-white text-xl rounded-2xl px-4 py-2 hover:bg-gray-800 transition"
+            onClick={onClick}
+        >
+            {text}
+        </button>
     )
 }
 
