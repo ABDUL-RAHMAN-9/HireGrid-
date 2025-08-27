bff046ec8223becae7aaf7320145aa23d559d463
Abdul Rahman <abdulrahman161004@gmail.com>
2025-08-27 23:14:54 +0530

    Added basic ContactInfo component structure
    - Created ContactInfo component as a functional component
    - Displayed static contact details: Email, Phone, and Address
    - Kept structure minimal (styling to be added in future)

---
+++ src/components/ContactInfo.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const ContactInfo = () =>
+{
+    return (
+        <div>
+            <div>Email: example@gmial.com</div>
+            <div>Phone: +1234567890</div>
+            <div>Address: xyz street , Bangalore</div>
+        </div>
+    )
+}
+
+export default ContactInfo
