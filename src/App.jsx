f6d8a8c1b31515b2096b961edf045949ecdbc71d
Abdul Rahman <abdulrahman161004@gmail.com>
2025-08-27 23:34:00 +0530

    Integrated NotFound page into routing.
    - Imported NotFound component in App.jsx.
    - Added wildcard (*) route under RootLayout to catch undefined paths.
    - Ensured users are redirected to NotFound page when route does not exist.
    - Improved app navigation flow with 404 handling.

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import RootLayout from './layout/RootLayout'
 import ContactLayout from './layout/ContactLayout'
 import ContactInfo from './components/ContactInfo'
 import ContactForm from './components/ContactForm'
+import NotFound from './components/NotFound'
 
 const App = () =>
 {
@@ -28,6 +29,7 @@ const App = () =>
           <Route path='info' element={<ContactInfo />} />
           <Route path='form' element={<ContactForm />} />
         </Route>
+        <Route path='*' element={<NotFound />} />
       </Route>
     )
   )
